aec15ad83035a119ccbd8f5a2db4a0563b19a8d5
Sotiris Panagiotou <sotirisp@Sotiriss-MacBook-Pro.local>
2019-04-07 00:41:49 +0100

    delete all

--- params.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2013-2015 The muecoin developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/muecoin/btcwallet/netparams"
-
-var activeNet = &netparams.MainNetParams
